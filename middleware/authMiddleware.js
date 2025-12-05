3ec89e284bb7ecd9b1302a929845c688e9987879
Dikshathakur20 <ranadikshathakur20@gmail.com>
2025-12-05 15:14:07 +0530

    update file

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,14 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
-  const token = process.env.TEST_TOKEN; // take token from .env
+  const header = req.headers["authorization"];
+  if (!header) {
+    return res.status(401).json({ message: "Authorization header missing" });
+  }
+
+  const parts = header.split(" ");
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    return res.status(400).json({ message: "Invalid Authorization format" });
+  }
+
+  const token = parts[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; 
+    req.user = decoded;   // ⭐ IMPORTANT
     next();
   } catch (err) {
     return res.status(403).json({ message: "Invalid or expired token" });
   }
 };
-
