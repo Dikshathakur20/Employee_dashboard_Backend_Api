158f2068600551a5fda3a93671998e09d169c1ee
Dikshathakur20 <ranadikshathakur20@gmail.com>
2025-12-11 13:18:50 +0530

    update files

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,17 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.query.token; // token from URL ONLY
+  const token = req.query.token;
 
   if (!token) {
     return res.status(401).json({ message: "Token is required in URL" });
   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid or expired token" });
+  if (token !== process.env.JWT_SECRET) {
+    return res.status(401).json({ message: "Invalid token" });
   }
+
+  next();
 };
